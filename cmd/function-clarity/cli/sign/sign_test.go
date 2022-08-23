c5ba30c0cfe83edda1c4537d18258714af48fea4
rmedvedo <rmedvedo@cisco.com>
2022-08-23 17:45:59 +0300

    Basic testing added for sign and verify

---
+++ cmd/function-clarity/cli/sign/sign_test.go
@@ -0,0 +1,58 @@
+package sign
+
+import (
+	"github.com/openclarity/function-clarity/cmd/function-clarity/cli/verify"
+	"github.com/openclarity/function-clarity/pkg/integrity"
+	"os"
+	"testing"
+)
+
+func TestSignIdentitySuccess(t *testing.T) {
+	const identity = "739356fc846067a7aa2e80fb6c3bf4e7f482ad80870cdf225ece9582576f3b2c"
+	const pathToTestingPair = "../../../../tasting_keys/"
+
+	funcDefer, err := mockStdin(t, "pass")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer funcDefer()
+
+	si, err := SignIdentity(pathToTestingPair+"cosign.key", identity)
+	if err != nil {
+		t.Fatalf("Error signing idenitity: %v", err)
+	}
+
+	err = verify.VerifyIdentity(pathToTestingPair+"cosign.pub", si, identity)
+	key, err := integrity.ReadFile(pathToTestingPair + "cosign.pub")
+	if err != nil {
+		t.Fatalf("Error, signature: %s cannot be varified by public key: %s, error: %v", si, key, err)
+	}
+}
+
+func mockStdin(t *testing.T, dummyInput string) (funcDefer func(), err error) {
+	t.Helper()
+
+	tmpfile, err := os.CreateTemp(t.TempDir(), t.Name())
+
+	if err != nil {
+		return nil, err
+	}
+
+	content := []byte(dummyInput)
+
+	if _, err := tmpfile.Write(content); err != nil {
+		return nil, err
+	}
+
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		return nil, err
+	}
+
+	oldOsStdin := os.Stdin
+	os.Stdin = tmpfile
+
+	return func() {
+		os.Stdin = oldOsStdin
+		os.Remove(tmpfile.Name())
+	}, nil
+}
---
+++ cmd/function-clarity/cli/verify/verify_test.go
@@ -0,0 +1,42 @@
+package verify
+
+import (
+	"github.com/openclarity/function-clarity/pkg/integrity"
+	"testing"
+)
+
+func TestSignIdentitySuccess(t *testing.T) {
+	const pathToTestingPair = "../../../../tasting_keys/"
+	const identity = "739356fc846067a7aa2e80fb6c3bf4e7f482ad80870cdf225ece9582576f3b2c"
+	const si = "MEQCIEvNqHFMor+DAlHVexDzrW4o81xGmIuznepLyMLzYw2mAiB6FdUT85eA6ZDMy6GByiVuQXDH03qCvAsGMxse5t0vxg=="
+
+	err := VerifyIdentity(pathToTestingPair+"cosign.pub", si, identity)
+	key, _ := integrity.ReadFile(pathToTestingPair + "cosign.pub")
+	if err != nil {
+		t.Fatalf("Error, signature: %s cannot be varified by public key: %s, error: %v", si, key, err)
+	}
+}
+
+func TestSignIdentityFailureOnWrongIdentity(t *testing.T) {
+	const pathToTestingPair = "../../../../tasting_keys/"
+	const identity_wrong = "4e725b9b14ccdf2149608747152c04b1ea6717049276302ffdfd30dacee99436"
+	const si = "MEQCIEvNqHFMor+DAlHVexDzrW4o81xGmIuznepLyMLzYw2mAiB6FdUT85eA6ZDMy6GByiVuQXDH03qCvAsGMxse5t0vxg=="
+
+	err := VerifyIdentity(pathToTestingPair+"cosign.pub", si, identity_wrong)
+	key, _ := integrity.ReadFile(pathToTestingPair + "cosign.pub")
+	if err == nil {
+		t.Fatalf("Error, signature: %s varified by public key: %s, despite it should fail verification because of wrong identity", si, key)
+	}
+}
+
+func TestVerifyIdentityFailureOnWrongPublicKey(t *testing.T) {
+	const identity = "739356fc846067a7aa2e80fb6c3bf4e7f482ad80870cdf225ece9582576f3b2c"
+	const pathToTestingPair = "../../../../tasting_keys/"
+	const si = "MEQCIEvNqHFMor+DAlHVexDzrW4o81xGmIuznepLyMLzYw2mAiB6FdUT85eA6ZDMy6GByiVuQXDH03qCvAsGMxse5t0vxg=="
+
+	err := VerifyIdentity(pathToTestingPair+"cosign_wrong.pub", si, identity)
+	key, _ := integrity.ReadFile(pathToTestingPair + "cosign_wrong.pub")
+	if err == nil {
+		t.Fatalf("Error, signature: %s varified by public key: %s, despite it should fail verification because of wrong public key", si, key)
+	}
+}
