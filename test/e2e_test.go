34722eb75323feb054434c5de1074297cd31bd5e
rmedvedo <rmedvedo@cisco.com>
2022-10-12 00:05:15 +0300

    E2e tests added for keyless scenarios

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -116,45 +116,6 @@ func shutdown() {
 	deleteS3Bucket(bucket)
 }
 
-//func TestCodeSignAndVerify(t *testing.T) {
-//	os.Setenv(integrity.ExperimentalEnv, "0")
-//	log.Printf("integrity.ExperimentalEnv: %v\n", os.Getenv(integrity.ExperimentalEnv))
-//	log.Printf("bucket: %v\n", bucket)
-//	viper.Set("privatekey", privateKey)
-//	switchConfiguration(false, publicKey)
-//	funcDefer, err := mockStdin(t, pass)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	defer funcDefer()
-//
-//	sbo := o.SignBlobOptions{
-//		SignBlobOptions: options.SignBlobOptions{
-//			Base64Output: true,
-//			Registry:     options.RegistryOptions{},
-//		},
-//	}
-//	err = sign.SignAndUploadCode(awsClient, "utils/testing_lambda", &sbo, ro)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	functionArn := initCodeLambda(t)
-//
-//	successTagValue := "Function signed and verified"
-//	success, timeout := findTag(t, functionArn, lambdaClient, "Function clarity result", successTagValue)
-//	if timeout {
-//		t.Fatal("test failed on timout, the required tag not added in the time period")
-//	}
-//	if !success {
-//		t.Fatal("test failure: no " + successTagValue + " tag in the signed function")
-//	}
-//	fmt.Println(successTagValue + " tag found in the signed function")
-//	deleteLambda(codeFuncName)
-//	log.Printf("finished pair code test... wait 1 min\n")
-//	deleteS3BucketContent(&bucket, []string{"function-clarity.zip"})
-//}
-
 func TestCodeSignAndVerifyKeyless(t *testing.T) {
 	os.Setenv(integrity.ExperimentalEnv, "1")
 	log.Printf("integrity.ExperimentalEnv: %v\n", os.Getenv(integrity.ExperimentalEnv))
@@ -190,6 +151,45 @@ func TestCodeSignAndVerifyKeyless(t *testing.T) {
 	fmt.Println(successTagValue + " tag found in the signed function")
 	deleteLambda(codeFuncName)
 	log.Printf("finished keyless code test... waiting...\n")
+	deleteS3BucketContent(&bucket, []string{"function-clarity.zip"})
+}
+
+func TestCodeSignAndVerify(t *testing.T) {
+	os.Setenv(integrity.ExperimentalEnv, "0")
+	log.Printf("integrity.ExperimentalEnv: %v\n", os.Getenv(integrity.ExperimentalEnv))
+	log.Printf("bucket: %v\n", bucket)
+	viper.Set("privatekey", privateKey)
+	switchConfiguration(false, publicKey)
+	funcDefer, err := mockStdin(t, pass)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer funcDefer()
+
+	sbo := o.SignBlobOptions{
+		SignBlobOptions: options.SignBlobOptions{
+			Base64Output: true,
+			Registry:     options.RegistryOptions{},
+		},
+	}
+	err = sign.SignAndUploadCode(awsClient, "utils/testing_lambda", &sbo, ro)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	functionArn := initCodeLambda(t)
+
+	successTagValue := "Function signed and verified"
+	success, timeout := findTag(t, functionArn, lambdaClient, "Function clarity result", successTagValue)
+	if timeout {
+		t.Fatal("test failed on timout, the required tag not added in the time period")
+	}
+	if !success {
+		t.Fatal("test failure: no " + successTagValue + " tag in the signed function")
+	}
+	fmt.Println(successTagValue + " tag found in the signed function")
+	deleteLambda(codeFuncName)
+	log.Printf("finished pair code test... wait 1 min\n")
 	deleteS3BucketContent(&bucket, []string{"function-clarity.zip"})
 }
 
