19d87de989927357beaeaf2f305ff038e1f26a2c
rmedvedo <rmedvedo@cisco.com>
2022-10-12 15:35:58 +0300

    E2e tests added for keyless scenarios

---
+++ test/e2e_test_keyless.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "starting..."
+cd ./aws_function_pkg
+go build
+mv ./aws_function_pkg ../test/aws_function
+echo "aws_function binary copied to test folder"
+
+cd ../test/utils
+go build testing_lambda.go
+echo "testing lambda built successfully"
+
+cd ../..
+cp ./run_env/utils/unified-template.template ./test/
+echo "stack template copied to test folder"
+
+echo "e2e tests started"
+cd ./test
+go test -timeout 30m -v -run TestCodeSignAndVerifyKeyless
+go test -timeout 30m -v -run TestImageSignAndVerifyKeyless
+
