926b4d6106831ffa10fe6a6dca298865c0ef4515
rmedvedo <rmedvedo@cisco.com>
2022-10-12 11:32:25 +0300

    E2e tests added for keyless scenarios

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -52,6 +52,7 @@ const (
 	imageFuncName        = "e2eTestImage"
 	role                 = "arn:aws:iam::813189926740:role/e2eTest"
 	imageUri             = "813189926740.dkr.ecr.us-east-2.amazonaws.com/securecn/serverless-scanner:busybox"
+	imageUri2            = "813189926740.dkr.ecr.us-east-2.amazonaws.com/securecn/serverless-scanner:05880d745f3159eca5ec03c732c80a235d1cd759"
 	publicKey            = "cosign.pub"
 	privateKey           = "cosign.key"
 	pass                 = "pass"
@@ -170,7 +171,7 @@ func TestCodeImageAndVerifyKeyless(t *testing.T) {
 		IDToken:          jwt,
 		RekorURL:         options.DefaultRekorURL,
 	}
-	err := s.SignCmd(ro, ko, options.RegistryOptions{}, nil, []string{imageUri}, "", "", true, "", "", "", false, false, "", false)
+	err := s.SignCmd(ro, ko, options.RegistryOptions{}, nil, []string{imageUri2}, "", "", true, "", "", "", false, false, "", false)
 	if err != nil {
 		t.Fatal(err)
 	}
